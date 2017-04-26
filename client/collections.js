7d7ea8a4e5dd0f09fba61e903fdb1f2e49646a22
Caleb Weeks <weeksseth@gmail.com>
2017-04-26 19:44:46 -0400

    fresh start

--- client/collections.js
+++
@@ -1,6 +0,0 @@
-//var remote = new DDP.connect("http://localhost:3030/");
-Events = new Mongo.Collection("events");
-Contacts = new Mongo.Collection("contacts");
-Churches = new Mongo.Collection('churches');
-Options = new Mongo.Collection('options');
-Groups = new Mongo.Collection("groups");
